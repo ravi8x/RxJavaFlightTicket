dfa65acb0c21eb128309c514238561bdfdd196a3
Ravi Tamada <ravi8x@gmail.com>
2018-03-01 23:46:23 +0530

    added base urls

---
+++ app/src/main/java/info/androidhive/flighttickets/app/Const.java
@@ -0,0 +1,11 @@
+package info.androidhive.flighttickets.app;
+
+/**
+ * Created by ravi on 01/03/18.
+ */
+
+public class Const {
+    // https://api.androidhive.info/json/airline-tickets.php
+    // https://api.androidhive.info/json/airline-tickets-price.php
+    public static final String BASE_URL = "";
+}
